2a650d64ccd8621a451ef8083fe3f4af23795776
Dave Seleno <dave@instantchannelinc.com>
2016-04-12 10:28:48 -0700

    remove ecto aliases

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule InstantElixir.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"]]
+    #["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+    # "ecto.reset": ["ecto.drop", "ecto.setup"]]
   end
 end
